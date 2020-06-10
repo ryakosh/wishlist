e026bd43163504393013a87d8918406eb2612f52
ryakosh <ryakosh@gmail.com>
2020-06-10 18:54:26 +0430

    Implement User and Wish models

---
+++ models.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"time"
+)
+
+type User struct {
+	ID                string     `json:"username" gorm:"type:varchar(64)" binding:"required"`
+	Email             string     `json:"email" gorm:"varchar(254)" binding:"required,email"`
+	IsEmailVerified   bool       `json:"is_email_verfied" binding:"-"`
+	Password          string     `json:"password" gorm:"varchar(256)" binding:"required"`
+	FirstName         string     `json:"first_name" gorm:"type:varchar(64)"`
+	LastName          string     `json:"last_name" gorm:"type:varchar(64)"`
+	Wishes            []Wish     `json:"wishes" binding:"-"`
+	FulfilledWishes   []Wish     `json:"fulfilled_wishes" gorm:"foreignkey:FulfilledBy" binding:"-"`
+	WantFulfillWishes []Wish     `json:"want_fulfill_wishes" gorm:"many2many:userswant_wishes" binding:"-"`
+	CreatedAt         *time.Time `binding:"-"`
+	UpdatedAt         *time.Time `binding:"-"`
+}
+
+type Wish struct {
+	ID          uint       `json:"id" binding:"-"`
+	UserID      string     `json:"username" binding:"-"`
+	Name        string     `json:"name" gorm:"type:varchar(256)" binding:"required"`
+	Description string     `json:"description" gorm:"type:text"`
+	Link        string     `json:"link" binding:"url"`
+	Image       string     `json:"image" binding:"url"`
+	FulfilledBy string     `json:"fulfilled_by" binding:"-"`
+	CreatedAt   *time.Time `binding:"-"`
+	UpdatedAt   *time.Time `binding:"-"`
+}
