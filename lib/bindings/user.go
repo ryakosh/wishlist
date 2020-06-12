3576d02d4f58ff28d27915ab4e6344d7e94918d8
ryakosh <ryakosh@gmail.com>
2020-06-12 13:00:51 +0430

    Implement User binding

---
+++ lib/bindings/user.go
@@ -0,0 +1,23 @@
+package bindings
+
+import "strings"
+
+// CuUser is used to Create and Update User models
+type CuUser struct {
+	ID        string `json:"username" binding:"required,username,max=64"`
+	Email     string `json:"email" binding:"required,email"`
+	Password  string `json:"password" binding:"required,min=8,max=256"`
+	FirstName string `json:"first_name" binding:"max=64"`
+	LastName  string `json:"last_name" binding:"max=64"`
+}
+
+// RUser is used to Read and Delete User models
+type RUser struct {
+	ID string `json:"username" binding:"required,username,max=64"`
+}
+
+// Canonicalize UserBinding's fields to be inserted in database
+func (b *CuUser) Canonicalize() {
+	b.FirstName = strings.TrimSpace(b.FirstName)
+	b.LastName = strings.TrimSpace(b.LastName)
+}
